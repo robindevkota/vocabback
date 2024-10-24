df5a23b7e6d09ae90cfcd7ecabc40a0f4a837053
Robin Devkota <robin.devkota@amniltech.com>
2024-10-24 20:00:39 +0545

    socket removed

--- server.js
+++ server.js
@@ -7,23 +7,9 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const userRoute = require('./routes/userRoute');
 const errorHandler = require('./middleware/errorMiddleware');
-const http = require('http');
-const socketIO = require('socket.io');
-const socketController = require('./controllers/socketController'); // Import socketController
-const socketRoutes = require('./routes/socketRoutes'); // Import socketRoutes
-const app = express();
-const httpServer = http.createServer(app);
 
-// Use the same http server instance for Socket.IO
-const io = new socketIO.Server(httpServer, {
-  cors: {
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3000', // Use an environment variable
-    methods: ['GET', 'POST'],
-  },
-});
-io.on('connection', (socket) => {
-  socketController.handleConnection(socket, io);
-})
+const app = express();
+
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -31,7 +17,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ['http://localhost:3000','https://api.dictionaryapi.dev','https://authz-app.vercel.app', 'http://192.168.18.10:3000'],
+    origin: ['http://localhost:3000', 'https://api.dictionaryapi.dev', 'https://authz-app.vercel.app', 'http://192.168.18.10:3000'],
     credentials: true,
   })
 );
@@ -39,7 +25,6 @@ app.use(
 // Routes
 app.use('/api', wordRoutes);
 app.use('/api/users', userRoute);
-app.use("/socket", socketRoutes);
 app.get('/', (req, res) => {
   res.send('Home Page');
 });
@@ -50,10 +35,7 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
-  httpServer.listen(PORT, () => {
+  app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 });
-
-
-exports.io = io; // Export the Socket.IO instance so it can be used in socketRoutes.js
